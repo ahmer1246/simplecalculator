847099146cfbf47d83c10aa90eef91bc22850546
ahmer1246 <ahmer1246@gmail.com>
2023-12-09 22:02:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let inputEl = document.getElementById("inp");
+let buttonEl = document.querySelectorAll("button")
+console.log(buttonEl)
+let string = " ";
+let arr = Array.from(buttonEl)
+arr.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=="="){
+            string=eval(string)
+            inputEl.value=string;
+        }
+        else if(e.target.innerHTML=="Del"){
+            string = string.substring(0 ,string.length - 1 );
+            inputEl.value =string;
+        }
+        else if(e.target.innerHTML == "AC"){
+           string =" "
+        }
+        else{
+            string+= e.target.innerHTML;
+            inputEl.value=string;
+        }
+
+    })
+})
+
